9d60fa74f722ed7931c7875b84cc19c2d1f231f4
Oksana Staeglazova <oksanik.med@gmail.com>
2021-07-04 23:47:40 +0300

    First task done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  "use strict";
+  let d = b**2-4*a*c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    let x = -b/(2*a);
+    arr = [x];
+  } else {
+    let x1 = (-b + Math.sqrt(d) )/(2*a); 
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr = [x1, x2];
+  }
   return arr; // array
 }
 
