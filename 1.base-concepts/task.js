0b3db7fd9a9ed8265f610137500097cc6fce4695
Oksana Staeglazova <oksanik.med@gmail.com>
2021-07-19 12:48:12 +0300

    *first task corrected;
    *Second task done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,11 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  "use strict";
+  let arr = [];
   let d = b**2-4*a*c;
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
     let x = -b/(2*a);
     arr = [x];
-  } else {
+  } else if ( d > 0) {
     let x1 = (-b + Math.sqrt(d) )/(2*a); 
     let x2 = (-b - Math.sqrt(d) )/(2*a);
     arr = [x1, x2];
@@ -15,8 +13,48 @@ function solveEquation(a, b, c) {
   return arr; // array
 }
 
+function monthDiff(date1, date2) {
+    let months;
+    months = (date2.getFullYear() - date1.getFullYear()) * 12;
+    months -= date1.getMonth();
+    months += date2.getMonth();
+    return months <= 0 ? 0 : months;
+}
+
+function validateParam(name, value) {
+  let validatedValue = value;
+  if (typeof value === 'string') {
+    validatedValue = Number(value);
+  } 
+  if (isNaN(validatedValue) || typeof validatedValue !== 'number') {
+    return `Параметр "${name}" содержит неправильное значение "${value}"`;
+  }
+  return validatedValue;
+}
+
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  percent = validateParam("Процентная ставка", percent);
+  if (typeof percent === 'string') {
+    return percent
+  }
+  contribution = validateParam("Начальный взнос", contribution);
+  if (typeof contribution === 'string') {
+    return contribution
+  }
+  amount = validateParam("Общая стоимость", amount);
+  if (typeof amount === 'string') {
+    return amount
+  }
+
   let totalAmount;
+  let creditBody = amount - contribution;   
+  let creditDuration = monthDiff(new Date(), date);
+  let monthPercent = percent / (12 * 100);
+  console.log(monthPercent);
+  let monthPayment = creditBody * (monthPercent + monthPercent / (((1 + monthPercent)**creditDuration) - 1));
+  console.log(monthPayment);
+  totalAmount = Math.round(monthPayment * creditDuration * 100) / 100;
 
   // код для задачи №2 писать здесь
 
