d228cce9b39fbf5905d19108f3fb68217a083424
Oksana Staeglazova <oksanik.med@gmail.com>
2021-07-21 00:23:47 +0300

    *task "Objects" done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  marks.forEach(mark => this.addMark(mark));
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+   for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  let avg = sum / this.marks.length; 
+  return avg;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
