2fd1cf2b808d48fce9ad7e6d3c2b2e4f091a5f02
Oksana Staeglazova <oksanik.med@gmail.com>
2021-07-20 14:53:06 +0300

    *initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((item, i) => item === arr2[i]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((number) => number > 0 && number % 3 === 0).map((number) => number * 10)
   return resultArr; // array
 }
