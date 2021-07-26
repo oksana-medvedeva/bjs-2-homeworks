1d3cd0bf38627058ae60c8f2cd5f7fda2d22a781
Oksana Staeglazova <oksanik.med@gmail.com>
2021-07-26 22:10:37 +0300

    *task for decorators done

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,68 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+    let idx = cache.findIndex((item) => item.hash === hash);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+       return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args);
+      cache.push({hash: hash, value: result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let isThrottled = false,
+  savedArgs,
+  savedThis;
+
+  function wrapper(...args) {
+    savedArgs = args;
+    savedThis = this;
+    if (isThrottled) {
+      return;
+    }
+
+    func.apply(this, savedArgs);
+    isThrottled = true;
+    setTimeout(() => {
+      isThrottled = false;
+    }, ms);
+  };
+  return wrapper
+  
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let isThrottled = false,
+  savedArgs,
+  savedThis;
+
+  function wrapper(...args) {
+    savedArgs = args;
+    savedThis = this;
+    wrapper.count += 1;
+    if (isThrottled) {
+      return;
+    }
+
+    func.apply(this, savedArgs);
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+    }, ms);
+  };
+  wrapper.count = 0;
+  return wrapper;
 }
